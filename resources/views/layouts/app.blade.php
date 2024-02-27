56c55cfeb03106d213f43f28b38d8839b58273dc
dbp26-ua <dbp26@alu.ua.es>
2024-02-27 10:04:20 +0100

    Añadida la página de perfil.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -60,7 +60,7 @@
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
+                                    <img src="{{ asset(Auth::user()->photo) }}" alt="Imagen de perfil" class="rounded-circle" width="30" height="30">
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="userDropdown">
---
+++ resources/views/users/profile.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+@extends('layouts.app')
+
+@section('content')
+    <h2>Información del perfil de {{ $user->name }}</h2>
+    <p>Email: {{ $user->email }}</p>
+
+    <p>Teléfono: {{ $user->phone }}</p>
+
+    <p>País: {{ $user->country }}</p>
+
+    <p>Población: {{ $user->city }}, {{ $user->zip_code }}</p>
+
+    <a class="btn btn-info" href="{{ route('user.games') }}">Listado de partidas</a>
+    <a class="btn btn-info" href="{{ route('user.boardgames') }}">Listado de juegos</a>
+@endsection
